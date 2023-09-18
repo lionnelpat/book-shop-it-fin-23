0f52f3e6631b26f113de16db131afa91690150d6
lionelhost <dplionnel@gmail.com>
2023-09-18 21:04:46 +0000

    book app creation

---
+++ book/views.py
@@ -0,0 +1,16 @@
+
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+def home_page(request):
+
+    return HttpResponse("Hello les Boss")
+
+
+def contact_page(request):
+
+    return HttpResponse("""
+    <h1 style='color:red;'> Page de contact </h1>
+""")
--- bookshop/urls.py
+++ bookshop/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from book.views import home_page, contact_page
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("",home_page),
+    path("contact", contact_page)
 ]
