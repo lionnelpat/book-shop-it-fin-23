8f3f3d0b42d03e9773aac51ad25672b8bfdb1be0
lionelhost <dplionnel@gmail.com>
2023-09-19 16:59:45 +0000

    update gitignore

--- book/views.py
+++ book/views.py
@@ -1,16 +1,14 @@
-
 from django.http import HttpResponse
 from django.shortcuts import render
+
 
 # Create your views here.
 
 def home_page(request):
-
     return HttpResponse("Hello les Boss")
 
 
 def contact_page(request):
-
     return HttpResponse("""
     <h1 style='color:red;'> Page de contact </h1>
 """)
